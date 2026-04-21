30a7a91c82d890fc4ed136525903e5c70b7e070c
rana yum <yumh@moravian.edu>
2026-04-21 00:54:10 -0400

    fixed redeploy.sh

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Navigate to project root
 cd /home/ec2-user/LiamRanaIsaac
+
+# Kill old process (force kill to make sure it dies)
+pkill -9 -f "WatchlistBot-1.0.0-jar-with-dependencies.jar"
+
+# Wait a moment for process to fully terminate
+sleep 2
 
 # Pull latest changes
 git pull origin main
 
 # Rebuild
 mvn clean package
-
-# Kill old process
-pkill -f "WatchlistBot-1.0.0-jar-with-dependencies.jar"
 
 # Restart bot
 nohup java -jar target/WatchlistBot-1.0.0-jar-with-dependencies.jar > bot.log 2>&1 &
