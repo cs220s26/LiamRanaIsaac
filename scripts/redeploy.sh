9bbf700881e9e0a49a81c95d20654b1df7857dac
Isaac <45981281+Izesik@users.noreply.github.com>
2026-04-21 13:23:25 -0400

    Remove Java declaration

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Set Java 21
-export JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto.x86_64
-export PATH=$JAVA_HOME/bin:$PATH
-
 # Pull latest changes
 sudo git pull origin main
-
 # Rebuild
 sudo mvn clean package
-
 # Restart bot
 sudo systemctl restart watchlistbot.service
-
-echo "Bot redeployed successfully!"
