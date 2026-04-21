bcf5c97dc83daf0507e0c50bcd27f427fb81c332
Isaac <45981281+Izesik@users.noreply.github.com>
2026-04-21 13:04:00 -0400

    update java version to use 21 in deployment

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -4,19 +4,7 @@
 sudo yum update -y
 
 # Install dependencies
-sudo yum install -y git redis6 maven java-26-amazon-corretto-devel
-
-# Configure Java/Maven to use Java 26
-sudo alternatives --set java /usr/lib/jvm/java-26-amazon-corretto.x86_64/bin/java
-sudo alternatives --set javac /usr/lib/jvm/java-26-amazon-corretto.x86_64/bin/javac
-
-# Set JAVA_HOME permanently for ec2-user
-echo 'export JAVA_HOME=/usr/lib/jvm/java-26-amazon-corretto.x86_64' >> /home/ec2-user/.bashrc
-echo 'export PATH=$JAVA_HOME/bin:$PATH' >> /home/ec2-user/.bashrc
-
-# Set JAVA_HOME for this script session
-export JAVA_HOME=/usr/lib/jvm/java-26-amazon-corretto.x86_64
-export PATH=$JAVA_HOME/bin:$PATH
+sudo yum install -y git redis6 maven java-21-amazon-corretto-devel
 
 # Clone repository
 git clone https://github.com/cs220s26/LiamRanaIsaac.git /LiamRanaIsaac
@@ -30,6 +18,6 @@ sudo systemctl enable redis6
 mvn clean package
 
 # Copy and start the systemd service
-cp watchlistbot.service /etc/systemd/system
+sudo cp watchlistbot.service /etc/systemd/system
 sudo systemctl enable watchlistbot
 sudo systemctl start watchlistbot
